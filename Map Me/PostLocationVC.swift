d5e2b56173d041e4b04016cabd9a4869004e9ecf
Scott Baumbich <sbaumbich777@gmail.com>
2016-02-01 07:57:43 -0600

    Geocoder implementation

--- Map Me/PostLocationVC.swift
+++ Map Me/PostLocationVC.swift
@@ -13,9 +13,11 @@ class PostLocationVC: UIViewController {
 
     
     @IBOutlet var titleLabel: UILabel!
-    @IBOutlet var InputTextField: UITextField!
+    @IBOutlet var inputTextField: UITextField!
     @IBOutlet var mapView: MKMapView!
     @IBOutlet var detailsLabel: UILabel!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,5 +29,27 @@ class PostLocationVC: UIViewController {
     }
     
     @IBAction func submitButton(sender: AnyObject) {
+        
+        let geocoder = CLGeocoder()
+        do {
+            geocoder.geocodeAddressString(self.inputTextField.text!) { (results, error) in
+                if let _ = error {
+                    self.activityIndicator.stopAnimating()
+                    let alert = UIAlertController(title: "Invalid Location", message:"Please enter a valid location. \n e.g Chicago, IL", preferredStyle: .Alert)
+                    alert.addAction(UIAlertAction(title: "Retry ", style: .Default) { _ in })
+                    self.presentViewController(alert, animated: true, completion: nil)
+                }  else if (results!.isEmpty){
+                    print("Not A valid location")
+                } else {
+                    print(results)
+                }
+            }
+        }
     }
 }
+
+
+
+
+
+
